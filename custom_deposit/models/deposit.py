16bd0679ad8cc1f32f108bfb1ffc77a091e042d6
midrovo <midrovo96@outlook.com>
2024-07-28 12:31:44 -0500

    fix-update: refactorizando el modulo

--- custom_deposit/models/deposit.py
+++ custom_deposit/models/deposit.py
@@ -88,26 +88,6 @@ class Deposit(models.Model):
             ('numero_cuenta', '=', vals['numero_cuenta'])
         ])
         
-        # if 'fecha' in vals and vals['fecha']:
-        #     try:
-        #         date_obj = fields.Datetime.from_string(vals['fecha'])
-        #         formatted_fecha = date_obj.strftime('%Y-%m-%d %H:%M:%S')
-
-        #         date_history = formatted_fecha
-
-        #         date_history_dt = local_tz.localize(datetime.strptime(date_history, '%Y-%m-%d %H:%M:%S'))
-
-        #         date_utc = date_history_dt.astimezone(pytz.utc)
-
-        #         history_date = date_utc.replace(tzinfo=None)
-                
-        #         vals['fecha'] = history_date
-        #         # Convertir la fecha a un objeto datetime
-        #         _logger.info(f"date_history_dt: {date_history_dt}, date_utc: {date_utc}, history_date mod: {history_date}")
-        #     except (ParserError, TypeError, ValueError) as e:
-        #         _logger.warning(f"Error parsing date: {e}")
-        #         history_date = fields.Datetime.now()
-        
         if deposito_existente:
             numero_de_papeleta = vals['papeleta_deposito']
             numero_de_cuenta = vals['numero_cuenta']
@@ -146,11 +126,7 @@ class Deposit(models.Model):
         
         _logger.info('HOLA ENTRA EN EL LOAD')
        
-        for record in records:
-            #record['fecha'] = datetime.strptime(record['fecha'], '%d/%m/%Y').strftime('%Y-%m-%d') 
-            
-            #fech = record['fecha']
-            #_logger.info(f'RECUPERANDO LA FECHA DEL RECORD { fech }')                       
+        for record in records:                      
             deposit_db = self.search([
                 ('numero_cuenta', '=', record['numero_cuenta']),
                 ('papeleta_deposito', '=', record['papeleta_deposito']),
