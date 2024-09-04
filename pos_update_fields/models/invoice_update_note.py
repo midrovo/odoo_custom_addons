63876d2aeb3fbc8fe8030d0d68acbda351a82e4e
midrovo <midrovo96@outlook.com>
2024-09-04 00:22:44 -0500

    fix-update: actualizando order line y payment screen

--- pos_update_fields/models/invoice_update_note.py
+++ pos_update_fields/models/invoice_update_note.py
@@ -5,10 +5,13 @@ _logger = logging.getLogger(__name__)
 
 
 class InvoiceUpdateNote(models.Model):
-    _inherit = "account.move"
+    _inherit = "pos.order"
 
     @api.model
     def get_note(self, nota):
+        invoice = super(InvoiceUpdateNote, self)._prepare_invoice_vals()
+
+        _logger.info(f'OBTENIENDO INVOICE >>> { invoice }')
         _logger.info(f'OBTENIENDO NOTA >>> { nota }')
 
         return nota
